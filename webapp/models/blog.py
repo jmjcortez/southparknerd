9a210f85be500077a0ac7834ce62f48e26768269
Jared Cortez <jared.cortez@xref.com>
2020-11-02 21:53:09 +1100

    Fixed migrations

--- webapp/models/blog.py
+++ webapp/models/blog.py
@@ -9,7 +9,7 @@ class Post(models.Model):
   date_posted = models.DateTimeField(auto_now=True)
   thumbnail_link = models.CharField(max_length=200)
   content = models.TextField()
-  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  id = models.UUIDField(default=uuid.uuid4, primary_key=True)
 
 class Thread(models.Model):
   post = models.ForeignKey(
